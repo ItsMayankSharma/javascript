437d7745e7db8937b6ccd2fbef4ae53c869b3673
ItsMayankSharma <itsmayanksharma02@gmail.com>
2026-06-04 21:14:26 +0530

    feat: initialize datatypes.js with variable declarations and data type documentation

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // using node js, not browser 
+
+let name = "Mayank"
+let age = 24
+let isLoggedIn = false
+
+// number => 2^53 - 1
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+console.log(typeof name);
