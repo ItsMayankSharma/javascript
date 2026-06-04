09e643f504e2f3cdb977f75fc7aff2716bfa9066
ItsMayankSharma <itsmayanksharma02@gmail.com>
2026-06-04 20:56:15 +0530

    feat: initialize variables and test scripts for JavaScript learning

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId = "11225544"
+let accountEmail = "mayank@gmail.com"
+var accountPass = "123456"
+accountCity = "Jaipur"
+console.log(accountId);
+console.table({accountId, accountEmail, accountPass, accountCity});
+
