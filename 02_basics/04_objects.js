aeb31e9b2c4fc204564fe1979927b69440cc191f
ItsMayankSharma <itsmayanksharma02@gmail.com>
2026-06-05 20:12:34 +0530

    feat: add examples for object creation, merging, and property access in JavaScript

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+//const tinderUser = new Object()   // singleton object
+
+const tinderUser = {}  // non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Mayank"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "mayank@gmail.com",
+    fullname: {
+        userFullName: {
+            firstName: "Mayank",
+            lastName: "Sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName)
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+const obj3 = { 5: "a", 6: "b" }
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+const obj4 = { ...obj1, ...obj2, ...obj3 }
+
+console.log(obj4);
+
+const arr = [
+    {
+        id: 1,
+        email: "mayank@gmail.com"
+    },
+    {
+        id: 1,
+        email: "mayank@gmail.com"
+    },
+    {
+        id: 1,
+        email: "mayank@gmail.com"
+    }
+]
+
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
