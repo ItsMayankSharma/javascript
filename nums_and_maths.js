54d74a882f8875edc0b09e464155ba87e9453dff
ItsMayankSharma <itsmayanksharma02@gmail.com>
2026-06-05 16:14:56 +0530

    refactor: comment out numeric logs and implement Math.random range logic

--- nums_and_maths.js
+++ nums_and_maths.js
@@ -1,18 +1,38 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const Balance = new Number(100)
 
-console.log(Balance);
+// console.log(Balance);
 
-console.log(Balance.toString().length)
-console.log(Balance.toFixed(1));
+// console.log(Balance.toString().length)
+// console.log(Balance.toFixed(1));
 
 
 const otherNumber = 123.8966    
 
-console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(4))
 
 const hundreds = 1000000000
 
-console.log(hundreds.toLocaleString('en-IN'))
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++++++++   MATHS   +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,3,6,8))
+// console.log(Math.max(4,3,6,8))
+
+
+console.log(Math.random() * 10)
+console.log(Math.floor(Math.random() * 10))
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
