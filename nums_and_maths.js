86f5f80421aba759ed4bb6bf4f19c12227b43978
ItsMayankSharma <itsmayanksharma02@gmail.com>
2026-06-05 16:02:46 +0530

    feat: add demonstration of JavaScript number methods and formatting techniques

---
+++ nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const Balance = new Number(100)
+
+console.log(Balance);
+
+console.log(Balance.toString().length)
+console.log(Balance.toFixed(1));
+
+
+const otherNumber = 123.8966    
+
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000000
+
+console.log(hundreds.toLocaleString('en-IN'))
